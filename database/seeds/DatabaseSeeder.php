844e20219529a7df7fe04b43b74fee518877ef01
Alisson Gomes <alisson.php@gmail.com>
2017-08-25 14:16:27 -0300

    rename roles table seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call('UsersTableSeeder');
-         $this->call('SeedRolesTable');
+         $this->call('RolesTableSeeder');
     }
 }
